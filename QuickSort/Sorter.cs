4cf1326b56611c34c21b0e3dce82db036ed67497
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2020-10-19 19:27:38 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ QuickSort/Sorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+#pragma warning disable SA1611
+
+namespace QuickSort
+{
+    public static class Sorter
+    {
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with quick sort algorithm.
+        /// </summary>
+        public static void QuickSort(this int[] array)
+        {
+            // TODO #1. Implement the method using a loop statements.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with recursive quick sort algorithm.
+        /// </summary>
+        public static void RecursiveQuickSort(this int[] array)
+        {
+            // TODO #2. Implement the method using recursion algorithm.
+            throw new NotImplementedException();
+        }
+    }
+}
