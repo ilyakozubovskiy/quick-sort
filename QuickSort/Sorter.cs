a898d8d8cf16ff88fcaf5f2a89d046f36c8a54e8
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2021-12-31 16:13:04 +0300

    Quick Sort

--- QuickSort/Sorter.cs
+++ QuickSort/Sorter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-// ReSharper disable InconsistentNaming
 #pragma warning disable SA1611
 
 namespace QuickSort
@@ -12,8 +11,62 @@ namespace QuickSort
         /// </summary>
         public static void QuickSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length < 2)
+            {
+                return;
+            }
+
+            int[] stack = new int[array.Length];
+            int top = -1;
+            stack[++top] = 0;
+            stack[++top] = array.Length - 1;
+
+            while (top >= 0)
+            {
+                int right = stack[top--];
+                int left = stack[top--];
+                int i = left, j = right;
+
+                while (i <= j)
+                {
+                    while (array[i] < array[(left + right) / 2])
+                    {
+                        i++;
+                    }
+
+                    while (array[j] > array[(left + right) / 2])
+                    {
+                        j--;
+                    }
+
+                    if (i <= j)
+                    {
+                        int tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+
+                        i++;
+                        j--;
+                    }
+                }
+
+                if (i < right)
+                {
+                    stack[++top] = i;
+                    stack[++top] = right;
+                }
+
+                if (left < j)
+                {
+                    stack[++top] = left;
+                    stack[++top] = j;
+                }
+            }
         }
 
         /// <summary>
@@ -21,8 +74,50 @@ namespace QuickSort
         /// </summary>
         public static void RecursiveQuickSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            RecursiveQuickSort(array, 0, array.Length - 1);
+        }
+
+        private static void RecursiveQuickSort(int[] array, int left, int right)
+        {
+            int i = left, j = right;
+
+            while (i <= j)
+            {
+                while (array[i] < array[(left + right) / 2])
+                {
+                    i++;
+                }
+
+                while (array[j] > array[(left + right) / 2])
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+            {
+                RecursiveQuickSort(array, left, j);
+            }
+
+            if (i < right)
+            {
+                RecursiveQuickSort(array, i, right);
+            }
         }
     }
 }
